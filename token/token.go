6272789d90a0691a49d5eaf04a6246c4e281b7f6
David Weiser <davidann@gmail.com>
2021-12-16 08:46:43 -0800

    add token

--- token/token.go
+++ token/token.go
@@ -1,3 +1,69 @@
 package token
 
-// todo: start at section 4.2, pg 43
+import "fmt"
+
+type TokenType int
+
+const (
+	// single character tokens
+	LEFT_PAREN TokenType = iota
+	RIGHT_PAREN
+	LEFT_BRACE
+	RIGHT_BRACE
+	COMMA
+	DOT
+	MINUS
+	PLUS
+	SEMICOLON
+	SLASH
+	STAR
+
+	// one or two character tokens
+	BANG
+	BANG_EQUAL
+	EQUAL
+	EQUAL_EQUAL
+	GREATER
+	GREATER_EQUAL
+	LESS
+	LESSS_EQUAL
+
+	//literals
+	IDENTIFIER
+	STRING
+	NUMBER
+
+	//keywords
+	AND
+	CLASS
+	ELSE
+	FALSE
+	FUN
+	FOR
+	IF
+	NIL
+	OR
+	PRINT
+	RETURN
+	SUPER
+	THIS
+	VAR
+	WHILE
+
+	EOF
+)
+
+type Token struct {
+	tokenType TokenType
+	lexeme    string
+	literal   interface{}
+	line      int
+}
+
+func MakeToken(typ TokenType, lexeme string, literal interface{}, lineno int) Token {
+	return Token{tokenType: typ, lexeme: lexeme, literal: literal, line: lineno}
+}
+
+func (t *Token) String() string {
+	return fmt.Sprintf("%v %v %v", t.tokenType, t.lexeme, t.literal)
+}
